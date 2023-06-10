7cc40443e0250e8f1c12ef3d84d5b74618eb973b
weq <weq+github@weq.no>
2023-06-10 16:05:45 +0200

    Additonal zones added.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,25 @@
+
 resource "azurerm_resource_group" "dns" {
-    name = "rg-miaka_dns-${var.environment}"
+    name = "rg-miaka_dns-prod"
     location = var.location
 }
 
 resource "azurerm_dns_zone" "miaka_info" {
   name = "miaka.info"
+  resource_group_name = azurerm_resource_group.dns.name
+}
+
+resource "azurerm_dns_zone" "dev_miaka_info" {
+  name = "dev.miaka.info"
+  resource_group_name = azurerm_resource_group.dns.name    
+}
+
+resource "azurerm_dns_zone" "test_miaka_info" {
+  name = "test.miaka.info"
+  resource_group_name = azurerm_resource_group.dns.name    
+}
+
+resource "azurerm_dns_zone" "staging_miaka_info" {
+  name = "staging.miaka.info"
   resource_group_name = azurerm_resource_group.dns.name
 }
