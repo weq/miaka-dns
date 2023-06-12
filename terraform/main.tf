57f9f4f18ae57c4ba0e37f19514292213ee83f34
weq <weq+github@weq.no>
2023-06-12 09:38:17 +0200

    NS records, attempt #1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,59 @@
 
 resource "azurerm_resource_group" "dns" {
-    name = "rg-miaka_dns-prod"
-    location = var.location
+  name = "rg-miaka_dns-prod"
+  location = var.location
+  tags = var.tags
 }
 
 resource "azurerm_dns_zone" "miaka_info" {
   name = "miaka.info"
   resource_group_name = azurerm_resource_group.dns.name
+  tags = var.tags
 }
 
+resource "azurerm_dns_ns_record" "dev" {
+  name                = "dev"
+  zone_name           = azurerm_dns_zone.miaka_info.name
+  resource_group_name = azurerm_resource_group.dns.name
+  ttl                 = 3600
+  # records = ["ns1.contoso.com.", "ns2.contoso.com."]
+  records = azurerm_dns_zone.dev_miaka_info.name_servers
+  tags = var.tags
+}
+
+resource "azurerm_dns_ns_record" "test" {
+  name                = "test"
+  zone_name           = azurerm_dns_zone.miaka_info.name
+  resource_group_name = azurerm_resource_group.dns.name
+  ttl                 = 3600
+  # records = ["ns1.contoso.com.", "ns2.contoso.com."]
+  records = azurerm_dns_zone.test_miaka_info.name_servers
+  tags = var.tags
+}
+
+resource "azurerm_dns_ns_record" "staging" {
+  name                = "staging"
+  zone_name           = azurerm_dns_zone.miaka_info.name
+  resource_group_name = azurerm_resource_group.dns.name
+  ttl                 = 3600
+  # records = ["ns1.contoso.com.", "ns2.contoso.com."]
+  records = azurerm_dns_zone.staging_miaka_info.name_servers
+  tags = var.tags
+}
 resource "azurerm_dns_zone" "dev_miaka_info" {
   name = "dev.miaka.info"
-  resource_group_name = azurerm_resource_group.dns.name    
+  resource_group_name = azurerm_resource_group.dns.name
+  tags = var.tags
 }
 
 resource "azurerm_dns_zone" "test_miaka_info" {
   name = "test.miaka.info"
   resource_group_name = azurerm_resource_group.dns.name    
+  tags = var.tags
 }
 
 resource "azurerm_dns_zone" "staging_miaka_info" {
   name = "staging.miaka.info"
   resource_group_name = azurerm_resource_group.dns.name
+  tags = var.tags
 }
